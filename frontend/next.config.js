ce66bd17649f49127254ae0ba4576bfec2fdccc6
DineshKatiyar-TrustAssist <dinesh.katiyar@trustassist.ai>
2025-12-08 21:04:54 -0800

    Configure async rewrites for API requests
    Added async rewrites to connect to local backend.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        // This connects to the backend running locally in the same container
+        destination: 'http://127.0.0.1:8000/api/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
-
